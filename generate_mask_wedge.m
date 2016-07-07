99bcf31b5d3d6492dec930ce51e1f8c4c1bed0e4
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2016-07-07 14:57:45 +0200

    "cleanup & documentation angle selection with tan"

--- generate_mask_wedge.m
+++ generate_mask_wedge.m
@@ -1,68 +1,68 @@
 function generate_mask_wedge(cfg)
-
-% cfg = [];
-cfg.ppd = 50;
+% Generate a gray mask with transparent wedge cutout in PNG file.
+%
+% INPUT
+% -----
+%   cfg : configuration struct with fields
+%       .imgSize : size of stimulus in px (default=screen size)
+%       .direction : orientation of wedge in deg (0=--, 45=/, 90=|, 135=\)
+%       .maskGrayValue : 8 bit gray value for mask (default=128)
+%       .minRadius : start of wedge from center in px (default=100px)
+%       .maxRadius : end of wedge from center in px (default=Inf)
+%       .filename : name of PNG file (default='auto')
 if ~exist('cfg', 'var'); cfg = []; end
-if ~isfield(cfg, 'ppd')
-    cfg.ppd = get_ppd();
+
+if ~isfield(cfg, 'imgSize')
+    cfg.screenId = 2;
+    cfg.imgSize = get(0, 'MonitorPositions');
+    cfg.imgSize = cfg.imgSize(cfg.screenId, [3 4]);
 end
 
-cfg.screenId = 2;
-cfg.screenSize = get(0, 'MonitorPositions');
-cfg.screenSize = cfg.screenSize(cfg.screenId, [3 4]);
-
-% % if ~isfield(cfg, 'center'); cfg.center = [0 0]; end % px
 if ~isfield(cfg, 'openingAngle'); cfg.openingAngle = 15; end % degree
-if ~isfield(cfg, 'direction'); cfg.direction = [90 ]; end % degree: 0=lower VM, 90=right HM
+if ~isfield(cfg, 'direction'); cfg.direction = [0]; end % degree: 0=HM, 90=VM, counter-clockwise
 if ~isfield(cfg, 'maskGrayValue'); cfg.maskGrayValue = 128; end % gray value (0-255)
-if ~isfield(cfg, 'minRadius'); cfg.minRadius = 2; end % vis. deg.
-if ~isfield(cfg, 'maxRadius'); cfg.maxRadius = 15; end % vis. deg.
-    
-openAngleRad = cfg.openingAngle * 2 * pi / 360;
+if ~isfield(cfg, 'minRadius'); cfg.minRadius = 100; end % px
+if ~isfield(cfg, 'maxRadius'); cfg.maxRadius = Inf; end % px
+if ~isfield(cfg, 'filename'); cfg.filename = 'auto'; end
 
-x = (1:cfg.screenSize(1)) - round(cfg.screenSize(1)/2);% + cfg.center(1);
-y = (1:cfg.screenSize(2)) - round(cfg.screenSize(2)/2);% + cfg.center(2);
+% opening angle in radians
+openAngleRad = cfg.openingAngle * pi / 180;
+
+% coordinate system
+x = (1:cfg.imgSize(1)) - round(cfg.imgSize(1)/2);
+y = (1:cfg.imgSize(2)) - round(cfg.imgSize(2)/2);
 [X,Y] = meshgrid(x, y);
-
 R = sqrt(X.^2 + Y.^2);
-Rdeg = R/cfg.ppd;
-Phi = atan2(X,Y);
+Phi = atan2(Y,X);
+
+% generate gray background
 background = uint8(ones(size(R)) * cfg.maskGrayValue);
 
-wedges = false(size(background));
-for ii = 1:length(cfg.direction)
-    directionRad = cfg.direction(ii) * 2 * pi / 360;
-    wedge = ...
-        Phi >= directionRad-openAngleRad/2 & ...
-        Phi <= directionRad+openAngleRad/2 & ...
-        Rdeg >= cfg.minRadius & ...
-        Rdeg <= cfg.maxRadius;
-    wedges = wedges | wedge;
+% create wedge
+directionRad = cfg.direction * pi / 180;
+dirSelector = sort(tan([directionRad-openAngleRad/2 directionRad+openAngleRad/2]));
+wedge = ...
+    tan(Phi) >= dirSelector(1) & ...
+    tan(Phi) <= dirSelector(2) & ...
+    R >= cfg.minRadius & ...
+    R <= cfg.maxRadius;
+wedge = ~wedge;
+
+
+if strcmp(cfg.filename, 'auto')
+    filename = sprintf('wedge_ori%udeg_%ux%upx_oang%gdeg_bg%d_r%g-%gpx.png', ...
+        cfg.direction, ...
+        cfg.imgSize(1), ...
+        cfg.imgSize(2), ...
+        cfg.openingAngle, ...
+        cfg.maskGrayValue, ...
+        cfg.minRadius, ...
+        cfg.maxRadius);
+else
+    filename = cfg.filename
 end
-wedges = ~wedges;
-figure
-subplot(2,1,1)
-imagesc(x,y,wedges)
-axis equal
-axis xy
-
-subplot(2,1,2)
-imagesc(x,y,background)
-axis equal
-axis xy
-
 % write to file
-directionString = sprintf('%d_', cfg.direction);
-directionString = directionString(1:end-1);
+imwrite(background, filename, 'Alpha', double(wedge))
 
-filename = sprintf('wedge_%s_o%d_m%d_r%g-%g.png', ...
-    directionString, ...
-    cfg.openingAngle, ...    
-    cfg.maskGrayValue, ...
-    cfg.minRadius, ...
-    cfg.maxRadius);
-
-imwrite(background, filename, 'Alpha', double(wedges))
-% imwrite(background, filename, 'Alpha', alphaMatrix)
 
 
