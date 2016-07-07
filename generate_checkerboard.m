ac0c897ce6c1d7502c0caf9d438d10d00ba19225
Joscha Schmiedt <joscha.schmiedt@esi-frankfurt.de>
2016-07-07 14:03:22 +0200

    "cleanup and documentation"

--- generate_checkerboard.m
+++ generate_checkerboard.m
@@ -1,33 +1,43 @@
 function generate_checkerboard(cfg)
-
-%
-cfg = [];
-cfg.ppd = 50;
+% Generate a radial black/white checkerboard PNG file
+% 
+% INPUT
+% -----
+%  cfg : struct for configuration with fields
+%       .radialCycles : number of complete cycles along radial direction
+%       .polarCycles : number of complete cycles along polar direction
+%       .contrast : gray value for contrast (0-1)
+%       .imgSize : size of image (px); default is smaller screen size
+%       .filename : filename of checkerboard PNG ('auto')
+% 
+% OUTPUT
+% ------
+%  A PNG image file that is fully transparent outside the checkerboard
+%  pattern. 
+%% defaults
 if ~exist('cfg', 'var'); cfg = []; end
-if ~isfield(cfg, 'ppd')
-    cfg.ppd = get_ppd();
-end
-
-if ~isfield(cfg, 'contrast');
-    cfg.contrast = 1;
-end
-
 
 % number of white/black circle pairs
 if ~isfield(cfg, 'radialCycles'); cfg.radialCycles = 10; end
 if ~isfield(cfg, 'polarCycles'); cfg.polarCycles = 8; end
 
+% contrast
 if ~isfield(cfg, 'contrast'); cfg.contrast = 1; end
 
-cfg.screenId = 2;
-cfg.screenSize = get(0, 'MonitorPositions');
-cfg.screenSize = cfg.screenSize(cfg.screenId, [3 4])
+% image size
+if ~isfield(cfg, 'imgSize')
+    cfg.screenId = 2;
+    cfg.screenSize = get(0, 'MonitorPositions');
+    cfg.screenSize = cfg.screenSize(cfg.screenId, [3 4]);
+    imgSize = min(cfg.screenSize);
+else
+    imgSize = cfg.imgSize;
+end
 
-%%
+% filename
+if ~isfield(cfg, 'filename'); cfg.filename = 'auto'; end
 
-
-imgSize = max(cfg.screenSize); % Change size
-
+%% create checkerboard pattern
 bgIndex = 128;
 blackIndex = round(128 + cfg.contrast * 127);
 whiteIndex = round(128 - cfg.contrast * 127);
@@ -40,10 +50,19 @@ radius = atan2(y,x);
 checks = ((1+sign(sin(radius*cfg.polarCycles)+eps) .* ...
     sign(sin(sqrt(x.^2+y.^2))))/2) * (blackIndex-whiteIndex) + whiteIndex;
 
+
 circle = x.^2 + y.^2 <= xylim^2;
 
 checks = uint8(circle .* checks + bgIndex * ~circle);
 
 alphaMask = double(circle);
 
-imwrite(checks, 'checkerboard.png', 'Alpha', alphaMask)
+%% store pattern in PNG file
+if strcmp(cfg.filename, 'auto')
+    saveFile = sprintf('checkerboard_%dpx_%grc_%gpc.png', ...
+        imgSize, cfg.radialCycles, cfg.polarCycles);
+else
+    saveFile = cfg.filename;
+end
+
+imwrite(checks, saveFile, 'Alpha', alphaMask)
